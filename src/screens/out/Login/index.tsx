1c8bdb9799124b9a9cf6425c39b866c900019ec1
FernandoChaves <fernando.chaves16@hotmail.com>
2021-06-22 19:58:27 -0300

    feat: created loginPage and add colors

---
+++ src/screens/out/Login/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import colors from '../../../styles/colors'
+
+export function LoginPage() {
+  return (
+    <View style={styles.container}>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.darkGrey
+  }
+})
