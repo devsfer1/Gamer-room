80ec0c3ec82d450eef69703930fed717f9aff108
FernandoChaves <fernando.chaves16@hotmail.com>
2021-06-22 20:13:34 -0300

    feat: added styled components modules

--- src/screens/out/Login/index.tsx
+++ src/screens/out/Login/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { Container } from './styles';
+
 import colors from '../../../styles/colors'
 
 export function LoginPage() {
   return (
-    <View style={styles.container}>
-    </View>
+    <Container>
+    
+    </Container>
   )
 }
 
---
+++ src/screens/out/Login/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native';
+
+import colors from '../../../styles/colors';
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${colors.darkenGrey}
+`
