e5f213d96d7bca9c3f0dd5621feba633f2afb8ac
FernandoChaves <fernando.chaves16@hotmail.com>
2021-06-22 20:29:04 -0300

    feat: added buttons and image to loginScreen

--- src/screens/out/Login/index.tsx
+++ src/screens/out/Login/index.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
-import { Container } from './styles';
+import { Container, Button, ButtonText } from './styles';
 
 import colors from '../../../styles/colors'
+
+const logoImg = require('../../../assets/images/logo.png')
 
 export function LoginPage() {
   return (
     <Container>
-    
+      <Image source={logoImg} width={10}/>
+      <Button >
+        <ButtonText>
+          LOGIN
+        </ButtonText>
+      </Button>
+      <Button >
+        <ButtonText>
+          REGISTER NOW
+        </ButtonText>
+      </Button>
     </Container>
   )
 }
--- src/screens/out/Login/styles.ts
+++ src/screens/out/Login/styles.ts
@@ -4,5 +4,20 @@ import colors from '../../../styles/colors';
 
 export const Container = styled.View`
   flex: 1;
-  background-color: ${colors.darkenGrey}
+  background-color: ${colors.darkenGrey};
+  align-items: center;
+`
+
+export const Button = styled.TouchableOpacity`
+  padding: 15px 0;
+  margin-bottom: 30px;
+  background-color: ${colors.darkGrey};
+  width: 70%;
+  text-align: center;
+  border-radius: 10px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`
+export const ButtonText = styled.Text`
+  color: #fff;
+  text-align: center;
 `
